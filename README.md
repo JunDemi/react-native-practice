0fecc12a9bad41042605382ee032daa842076824
ParkJungWook <jungwook3176@gmail.com>
2025-05-23 18:17:56 +0900

    Start React Native App

---
+++ README.md
@@ -0,0 +1,14 @@
+### React Native Practice
+
+    //React Native CLI
+    npx react-native init todo-app --template react-native-template-typescript
+    npm start
+
+    //Expo 
+    npx create-expo-app todo-app --template expo-template-blank-typescript
+    npx expo start
+
+### TODO APP
+
+    //zustand
+    npm i zustand
---
+++ src/store/useTodoStore.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+export interface Todo {
+  id: string;
+  text: string;
+  done: boolean;
+}
+
+interface TodoState {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  toggleTodo: (id: string) => void;
+  removeTodo: (id: string) => void;
+}
+
+export const useTodoStore = create<TodoState>((set) => ({
+  todos: [],
+  addTodo: (text) =>
+    set((state) => ({
+      todos: [...state.todos, { id: Date.now().toString(), text, done: false }],
+    })),
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      ),
+    })),
+  removeTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+}));
